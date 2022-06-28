78216175a1240d377e0b153fb528bff9974dd098
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-06-28 15:22:35 +0700

    Delete _ownerbilek.js

--- plugins/_ownerbilek.js
+++
@@ -1,9 +0,0 @@
-let fs = require('fs')
-let handler = async (m) => {
-let stc = fs.readFileSync('./src/tag.webp')
-conn.fakeReply(m.chat, stc, '6289625556161@s.whatsapp.net', '*Kenapa Tag Owner Saya*')
-}
-handler.customPrefix = /^(@6289625556161)$/i
-handler.command = new RegExp
-
-module.exports = handler
